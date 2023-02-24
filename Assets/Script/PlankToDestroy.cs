03d9553bb2e03258c1979bfe74a1b12aceaaf073
Barthelemy Mahe <barthelemy.mahe.pro@gmail.com>
2023-02-24 15:09:39 +0100

    crowbar

--- Assets/Script/PlankToDestroy.cs
+++ Assets/Script/PlankToDestroy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlankToDestroy : MonoBehaviour
 {
+    [SerializeField] GameObject Door1;
+    [SerializeField] GameObject Door2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,16 @@ public class PlankToDestroy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name);
+        if(collision.gameObject.tag == "Weapon")
+        {
+            Door1.GetComponent<Rigidbody>().freezeRotation = false;
+            Door2.GetComponent<Rigidbody>().freezeRotation = false;
+            Destroy(gameObject);
+        }
     }
 }
