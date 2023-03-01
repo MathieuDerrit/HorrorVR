baaf5554a898e184d6930ee5e8d0aecc502e0ad7
Mathieu Derrit <mathieu.derrit@gmail.com>
2023-03-01 12:50:25 +0100

    Dolls level

--- Assets/Script/Temp/InventoryVR.cs
+++ Assets/Script/Temp/InventoryVR.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class InventoryVR : MonoBehaviour
 {
+
+    [SerializeField] InputActionReference _input;
+
+
     public GameObject Inventory;
     public GameObject Anchor;
     bool UIActive;
@@ -14,21 +19,31 @@ public class InventoryVR : MonoBehaviour
     public InputHelpers.Button button;
 
 
+
+
     private void Start()
     {
         Inventory.SetActive(false);
         UIActive = false;
+
+        _input.action.started += OnFireInput;
+    }
+
+    private void OnFireInput(InputAction.CallbackContext obj) {
+ Debug.Log("Pressed !!! ");
     }
 
     private void Update()
-    {
+    {  Debug.Log("U ");
+        /*
         bool pressed;
         
         rightHand.inputDevice.IsPressed(button, out pressed);
 
 
  
-        if (pressed) {
+        if (pressed) {*/
+        if (_input.action.IsPressed()) {
             Debug.Log("Pressed - " + button);
 
             UIActive = !UIActive;
