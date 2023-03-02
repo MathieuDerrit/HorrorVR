0c57466349e675396d650ba08a829d761c337ba2
Custromix <75646944+Custromix@users.noreply.github.com>
2023-03-02 20:18:03 +0100

    End flashlight

--- Assets/Script/GamePlay/FlashLight.cs
+++ Assets/Script/GamePlay/FlashLight.cs
@@ -15,6 +15,79 @@ public class FlashLight : HandSelected
     [SerializeField]
     private Light _SpotLight;
 
+    [SerializeField]
+    private GameObject _Keys;
+
+    [SerializeField]
+    private float _SphereRadius;
+
+    [SerializeField]
+    private float _MaxDistance;
+
+    [SerializeField]
+    private LayerMask _LayerMask;
+
+    private Vector3 _Direction;
+    private float _CurrentHitDistance;
+    private Vector3 _CurrentHitPosition;
+
+    private GameObject _BoxCollider;
+
+
+    public Mesh _Sphere;
+
+    public GameObject Key;
+    void Start()
+    {
+    }
+
+    protected override void UpdateChild()
+    {
+        if (_SpotLight.enabled)
+        {
+            if (_BoxCollider == null)
+            {
+                CreateSphere();
+            }
+            _Direction = transform.forward;
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, _Direction);
+
+            if (Physics.Raycast(ray, out hit, _MaxDistance, _LayerMask))
+            {
+                //_BoxCollider.GetComponent<DetectChestKey>().DisableOutline();
+                
+                _Keys = hit.transform.gameObject;
+                _CurrentHitDistance = hit.distance;
+                _CurrentHitPosition = hit.point;
+
+                SetSphereCollider();
+            }
+            else
+            {
+                _CurrentHitDistance = _MaxDistance;
+                print(_CurrentHitDistance);
+                _Keys = null;
+            }
+        }
+        else
+        {
+            if (_BoxCollider != null)
+            {
+                DetectChestKey._Key.GetComponent<Outline>().enabled = false;
+                Destroy(_BoxCollider);
+            }
+            
+        }
+        
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Debug.DrawLine(transform.position, transform.position + _Direction * _CurrentHitDistance, Color.red);
+    }
+
     protected override void OnLeftHandGrap()
     {
         if (_PrimaryLeftBtn.action.WasPressedThisFrame())
@@ -43,5 +116,35 @@ public class FlashLight : HandSelected
                 _SpotLight.enabled = false;
             }
         }
+    }
+
+    private float ClampRadius(float distance)
+    {
+        return distance * 0.5f / 10f;
+    }
+
+    private float ClampRadiusSphereRender(float distance)
+    {
+        return distance * 4.5f / 10f;
+    }
+
+    private void SetSphereCollider()
+    {
+        _BoxCollider.transform.SetPositionAndRotation(_CurrentHitPosition, Quaternion.identity);
+        _BoxCollider.transform.localScale = new Vector3(ClampRadiusSphereRender(_CurrentHitDistance), ClampRadiusSphereRender(_CurrentHitDistance), ClampRadiusSphereRender(_CurrentHitDistance));
+
+        //SphereColliderGO.radius = ClampRadius(_CurrentHitDistance);
+    }
+
+    private void CreateSphere()
+    {
+        _BoxCollider = new GameObject("Cool GameObject made from Code");
+        _BoxCollider.transform.SetPositionAndRotation(_CurrentHitPosition, Quaternion.identity);
+        SphereCollider SphereColliderGO = _BoxCollider.AddComponent<SphereCollider>();
+        MeshFilter MeshFilterGO = _BoxCollider.AddComponent<MeshFilter>();
+        //MeshFilterGO.mesh = _Sphere;
+        MeshRenderer MeshRendererGO = _BoxCollider.AddComponent<MeshRenderer>();
+        _BoxCollider.AddComponent<DetectChestKey>();
+        SphereColliderGO.isTrigger = true;
     }
 }
