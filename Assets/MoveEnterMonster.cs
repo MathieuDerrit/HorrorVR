9b87db6dcb4691f7348963c2689aeadda83cd9f8
HugoTRZ <91878322+HugoTRZ@users.noreply.github.com>
2023-03-03 05:40:21 +0100

    Fix Audio + Cinematic Enter Monster

--- Assets/MoveEnterMonster.cs
+++ Assets/MoveEnterMonster.cs
@@ -7,17 +7,19 @@ public class MoveEnterMonster : MonoBehaviour
     public Vector3 targetPosition;
     public float speed = 2;
 
-    public AudioSource audioSource;
-
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.Play();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (transform.position == targetPosition){
+            Destroy(gameObject);
+        }
     }
 }
